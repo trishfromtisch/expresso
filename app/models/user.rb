8d9793b6f52b278f08d190f6056803b94a7c03e9
trishfromtisch <parlaws@gmail.com>
2014-11-10 12:15:28 -0500

    autocomplete without map is functional

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	has_many :coveted_coffees
 	has_many :posts
 	has_many :coffee_bars, through: :posts
-	has_many :follows
+	has_many :relationships
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 	validates :name, presence: true
